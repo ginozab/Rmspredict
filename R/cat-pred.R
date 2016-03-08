1f3be167629c3ca9027d78681e4a60f77c463df8
ginozab <brandonginoza34@gmail.com>
2016-03-08 17:21:18 -0500

    working on cat-pred.R

--- R/cat-pred.R
+++ R/cat-pred.R
@@ -7,20 +7,28 @@
 #' desired machine learning algorithm
 
 cat_predict <- function(data, method, pred) {
-  train <- data[!(data$program==pred),]
+  train1 <- data[!(data$program==pred),]
+  print(summary(train1))
+  train <- Rmspredict::cat_sample(train1, "up", "range")
+  train$Class <- NULL
+  print(summary(train))
   test <- data[(data$program==pred),]
+  test <- expandRows(test, 4)
   test$program <- NULL
   train$program <- NULL
-  testResults <- dplyr::select(test, source, range)
+  testRanges <- dplyr::select(test, source, range)
   #testEvoResults <- data.frame("program" = testEvo$program, "range" = testEvo$range)
   test$range <- NULL
 
   inTraining <- trainControl(method = "repeatedcv", number = 10, repeats = 1)
   model <- train(range~., data=train, trControl=inTraining, method=method)
-  predict <- predict(model, newdata = test)
-  test <- dplyr::full_join(test, testResults, by="source")
+  test$prediction <- predict(model, newdata = test)
+  #print(predict)
+  test <- dplyr::full_join(test, testRanges, by="source")
+  #test["prediction"] <- predict
   #test["range"] <- testResults$range
   #print(testEvo$range)
-  cmCat <- confusionMatrix(predict, test$range)
-  return(cmCat$overall["Accuracy"])
+  #cmCat <- confusionMatrix(predict, test$range)
+  #return(cmCat$overall["Accuracy"])
+  return(test)
 }
