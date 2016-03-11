2b2f9ec5c38b216770668dc8b5a9b096fcffc04b
ginozab <brandonginoza34@gmail.com>
2016-03-10 23:47:11 -0500

    New categorical predict file

---
+++ R/cat-predict.R
@@ -0,0 +1,23 @@
+# Categorical Prediction Basic
+
+#' FUNCTION: cat_prediction
+#'
+#' This function is supports basic categorical prediction
+#'
+
+cat_prediction <- function(data, method, pred, kfolds = 10, repeats = 1) {
+  train <- data[!(data$program==pred),]
+  test <- data[(data$program==pred),]
+  test$program <- NULL
+  train$program <- NULL
+  testRanges <- dplyr::select(test, source, range)
+  test$range <- NULL
+
+  inTraining <- trainControl(method = "repeatedcv", number = kfolds, repeats = repeats)
+  model <- train(range~., data=train, trControl=inTraining, method=method)
+  test$prediction <- predict(model, newdata = test)
+  test <- dplyr::full_join(test, testRanges, by="source")
+  print(test$range)
+  cmCat <- confusionMatrix(test$prediction, test$range)
+  return(cmCat$overall["Accuracy"])
+}
