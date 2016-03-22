548ac5df479bf6fdd7c4edb2ddac91cc790af8fb
ginozab <brandonginoza34@gmail.com>
2016-03-22 14:47:27 -0400

    benchmark function now returns both desired evaluation metrics for both classification and regression models

--- R/bench.R
+++ R/bench.R
@@ -21,7 +21,14 @@ benchmark <- function(trials, methods, programs,
                                              methods = methods, sampling = sampling, type = type, repeats=repeats)
   }
   df <- do.call("rbind", data.list)
-  df$value <- as.numeric(df$value)
+  if (type == "classification") {
+    df$Accuracy <- as.numeric(df$Accuracy)
+    df$Kappa <- as.numeric(df$Kappa)
+  }
+  else {
+    df$RMSE <- as.numeric(df$RMSE)
+    df$MAE <- as.numeric(df$MAE)
+  }
   return(df)
 }
 
@@ -35,24 +42,60 @@ benchmark <- function(trials, methods, programs,
 #methods <- list("svmRadial", "parRF", "majority")
 
 pred_bench <- function(trials, pred, data, methods, sampling, type, kfolds, repeats) {
-  print(pred)
+  #print(pred)
   acc.output <- matrix(ncol = length(methods), nrow = trials)
+  kap.output <- matrix(ncol = length(methods), nrow = trials)
   for (j in 1:length(methods)) {
-    print(methods[[j]])
+    #print(methods[[j]])
     for (i in 1:trials) {
       acc.output[i,j] <- Rmspredict::pred_frame(method=methods[[j]], pred=pred, data=data,
-                                                sampling=sampling, type=type, kfolds=kfolds, repeats=repeats)
+                                                sampling=sampling, type=type, kfolds=kfolds, repeats=repeats, eval = 1)
+      kap.output[i,j] <- Rmspredict::pred_frame(method=methods[[j]], pred=pred, data=data,
+                                                sampling=sampling, type=type, kfolds=kfolds, repeats=repeats, eval = 2)
+
+      print(pred)
+      print(methods[[j]])
+      print("Eval One")
       print(acc.output[i,j])
+      print("Eval Two")
+      print(kap.output[i,j])
+      print("")
     }
   }
 
-  Program <- rep(pred, length(acc.output[[j]]))
-  acc.output <- cbind(Program, acc.output)
-  colnames(acc.output) <- c("Program", methods)
-  acc.output <- as.data.frame(acc.output)
-  acc.output <- melt(acc.output, id = "Program")
+  acc.output <- data_prep(acc.output, pred, j, methods)
+  acc.output <- melt(acc.output, id = c("Trial", "Program"))
 
-  return(acc.output)
+
+  kap.output <- data_prep(kap.output, pred, j, methods)
+  kap.output <- melt(kap.output, id = c("Trial", "Program"))
+
+
+  if (type == "classification") {
+    colnames(acc.output)[which(names(acc.output) == "value")] <- "Accuracy"
+    colnames(kap.output)[which(names(kap.output) == "value")] <- "Kappa"
+  }
+  else {
+    colnames(acc.output)[which(names(acc.output) == "value")] <- "RMSE"
+    colnames(kap.output)[which(names(kap.output) == "value")] <- "MAE"
+  }
+
+  output <- dplyr::full_join(acc.output, kap.output, by=c("Trial", "Program", "variable"))
+
+  return(output)
+}
+
+#' FUNCTION: data_prep
+#'
+#' This function gets the data in tidy format after trials have been run
+
+data_prep <- function(data, pred, j, methods) {
+  Program <- rep(pred, length(data[[j]]))
+  data <- cbind(Program, data)
+  data <- cbind(order=seq(nrow(data)), data)
+  colnames(data) <- c("Trial", "Program", methods)
+  data <- as.data.frame(data)
+  return(data)
 }
 
 #' FUNCTION: pred_frame
@@ -60,32 +103,65 @@ pred_bench <- function(trials, pred, data, methods, sampling, type, kfolds, repe
 #' This function provides a frame for all possible prediction
 #' configurations
 
-pred_frame <- function(method, pred, data, sampling, type, kfolds, repeats) {
+pred_frame <- function(method, pred, data, sampling, type, kfolds, repeats, eval) {
   if (type == "classification") {
     if (method == "majority") {
-      return(cat_majority(data, pred))
+      retlist <- cat_majority(data, pred)
+
+      if (eval == 1) {
+        return(matrix(unlist(retlist["acc"]), ncol = 1, byrow = TRUE))
+      }
+      else {
+        return(matrix(unlist(retlist["kap"]), ncol = 1, byrow = TRUE))
+      }
+
     }
-    else if (method == "random") {
-      return(cat_rand(data,pred))
-    }
+    #else if (method == "random") {
+    #  retlist <- cat_rand(data, pred)
+
+    #  if (eval == 1) {
+    #    return(matrix(unlist(retlist["acc"]), ncol = 1, byrow = TRUE))
+    #  }
+    #  else {
+    #    return(matrix(unlist(retlist["kap"]), ncol = 1, byrow = TRUE))
+    #  }
+
+    #}
     else {
-      return(cat_prediction(data,method,pred, kfolds, repeats))
+      retlist <- cat_prediction(data,method,pred, kfolds, repeats)
+
+      if (eval == 1) {
+        return(matrix(unlist(retlist["acc"]), ncol = 1, byrow = TRUE))
+      }
+      else {
+        return(matrix(unlist(retlist["kap"]), ncol = 1, byrow = TRUE))
+      }
+
     }
   }
+
+
   else if (type == "regression") {
     if (method == "average") {
       avg_preds <- reg_average_pred(data,pred)
-      rmse <- matrix(unlist(avg_preds["rmse"]), ncol = 1, byrow = TRUE)
-      mae <- matrix(unlist(avg_preds["mae"]), ncol = 1, byrow = TRUE)
-      return(rmse)
+      if (eval == 1) {
+        return(matrix(unlist(avg_preds["rmse"]), ncol = 1, byrow = TRUE))
+      }
+      else {
+        return(matrix(unlist(avg_preds["mae"]), ncol = 1, byrow = TRUE))
+      }
     }
     else {
       num_preds <- num_predict(data,method,pred)
-      rmse <- matrix(unlist(num_preds["rmse"]), ncol = 1, byrow = TRUE)
-      mae <- matrix(unlist(num_preds["mae"]), ncol = 1, byrow = TRUE)
-      return(rmse)
+      if (eval == 1) {
+        return(matrix(unlist(num_preds["rmse"]), ncol = 1, byrow = TRUE))
+      }
+      else {
+        return(matrix(unlist(num_preds["mae"]), ncol = 1, byrow = TRUE))
+      }
     }
   }
+
 
   else {
     stop("type needs to be either 'classification' or 'regression'")
